450a50d13203add73f836cee5ed423112035b37a
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-02 22:26:01 +0600

    variable .js updated

---
+++ 02.VariableInJS/variable.js
@@ -0,0 +1,8 @@
+var firstName = "Salman",
+      lastName = "Farshe",
+      age = 21,
+      presentAddress = "Ashulia, Saver, Dhaka",
+      permanentAddress = "Nazipur, Naogoan, Rajshahi",
+      booldGroup = "B+";
+var CV = "My name is " + firstName + " " + lastName + ". My age is " + age + ". My present address is " + presentAddress + " and my permanent address is " + permanentAddress + ". My blood group is " + booldGroup  + ".";
+console.log(CV);
