3f63f34de6a6a984a569b99a597291b0915782cc
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-02 23:54:11 +0600

    number.js file updated

---
+++ 03.NumberInJavascript/numbers.js
@@ -0,0 +1,11 @@
+// number types
+var integerNumber = 25;
+var floatNumber = 25.639;
+var stringASaNumber = "55546546This text will never printed with number constractor";
+console.log(integerNumber);
+console.log(floatNumber);
+console.log(stringASaNumber);
+console.log(parseInt(stringASaNumber));
+console.log(Number.parseFloat(integerNumber));
+var numberConstractor = Number("22")
+console.log(numberConstractor);
