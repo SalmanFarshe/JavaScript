2d9f2e9ac5144602343ba0a3e48875402fcd8427
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-03 00:19:43 +0600

    number.js file up to date

--- 03.NumberInJavascript/numbers.js
+++ 03.NumberInJavascript/numbers.js
@@ -12,3 +12,10 @@ console.log(numberConstractor);
 // max value cheacker
 console.log(Number.MAX_VALUE); //this amount off value can store in javascript
 console.log(Number.MIN_VALUE); //this is the most minimal value of a number storage
+
+console.log(Infinity);
+console.log(1 / 0);//it will be infinity
+console.log("kkk" * 5); // it will be a NAN output, NAN = NOT A NUMBER
+
+var emptyVariable = Number(""); //empty number, it will be 0
+console.log(emptyVariable);
