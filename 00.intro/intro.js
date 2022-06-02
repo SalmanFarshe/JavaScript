43cdc9ec261f8240184b0dfe602fa00525502e46
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-02 22:13:04 +0600

    i done the 1st console log

---
+++ 00.intro/intro.js
@@ -0,0 +1,5 @@
+console.log(
+	"the jurney is started.... Best off luck... you will do it...Insah allah"
+);
+console.log(5)
+console.log("hello gyes my faviourite number is " + 1 + " and " + 0)
---
+++ 01.HelloWorld/hello-world.js
@@ -0,0 +1,3 @@
+// print a string
+console.log("I Love Allah"); // log alwys print into the console...
+console.log("My faviorite nomber is " + 0 + " and " + 1 );
