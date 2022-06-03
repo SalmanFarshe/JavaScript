fd757de7d6a807b147d612b11254a9c340b3521a
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-03 21:35:10 +0600

    octal and hexal updated

---
+++ 07.OctalAndHexal/octalandhexal.js
@@ -0,0 +1,4 @@
+var hexalNumber = 0xA; // for storing hexal nomber you have to use "0X" then store the number
+console.log(hexalNumber);
+var octalNumber = 0157472024; // for storing octal nomber you have to use "0" then store the number
+console.log(octalNumber);
