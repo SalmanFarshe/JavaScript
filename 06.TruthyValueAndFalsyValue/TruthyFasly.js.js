e29feca1abd5ebfc9743db1767cdf0a1007ca48c
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-05 14:02:12 +0600

    two file updated

--- 06.TruthyValueAndFalsyValue/TruthyFasly.js.js
+++ 06.TruthyValueAndFalsyValue/TruthyFasly.js.js
@@ -6,7 +6,6 @@ console.log(Boolean(x)); //falsy value
 console.log(Boolean(null)); //falsy value
 console.log(Boolean("")); //falsy value
 console.log(Boolean(undefined)); //falsy value
-
 console.log(Boolean(Infinity)); //truthy value
 console.log(Boolean(-Infinity)); //truthy value
 console.log(Boolean(1)); //truthy value
--- 07.OctalAndHexal/octalandhexal.js
+++ 07.OctalAndHexal/octalandhexal.js
@@ -1,4 +1,7 @@
-var hexalNumber = 0xA; // for storing hexal nomber you have to use "0X" then store the number
+var hexalNumber = 0xa; // for storing hexal nomber you have to use "0X" then store the number
 console.log(hexalNumber);
 var octalNumber = 0157472024; // for storing octal nomber you have to use "0" then store the number
 console.log(octalNumber);
+var str = "i am salman farshe"
+console.log(str);
+// remind that 
