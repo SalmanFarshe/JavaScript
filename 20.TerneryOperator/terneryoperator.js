ccd709f53f1d377e4e60bcd5a8dd633930663e41
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-23 13:21:39 +0600

    ternery operator

---
+++ 20.TerneryOperator/terneryoperator.js
@@ -0,0 +1,12 @@
+var a = 15,
+  b = 20;
+// if (a < b) {
+//     console.log( "B is biggest")
+// } else {
+//     console.log("A is the biggest");
+// }
+
+// ternery operator
+// condition ? statement as true value : statement as false value
+
+console.log(a < b ? "B is the biggest number" : "A is the biggest number"); //  -----?----:-----
