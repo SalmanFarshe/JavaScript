7edab527ab2acb03a59c41325136a87402e256f0
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-24 23:46:34 +0600

    do while loop

---
+++ 23.doWhileLoop/do-while.js
@@ -0,0 +1,9 @@
+var is_Running = false;
+do {
+  console.log("i am from finite do while loop");
+} while (is_Running); // it will apply finity times
+
+is_Running = true;
+do {
+  console.log("i am from infinite do while loop");
+} while (is_Running); // it will apply infinity times
