fa7741ce378d9f0b9d6fbf0705addeaa2a69cf16
SalmannFarshee <salmanzihad9024@gmail.com>
2022-06-24 23:02:16 +0600

    whileloop updated

--- 22.whileLoop/tempCodeRunnerFile.js
+++ 22.whileLoop/tempCodeRunnerFile.js
@@ -1,10 +1,10 @@
-var a = true
-    var b = 0;
-    while (a) {
-        b++
-        if (b <= 10) {
-            console.log(b);
-        } else {
-            a = false
-        }
-    }
+var a = true;
+var b = 0;
+while (a) {
+  b++;
+  if (b <= 10) {
+    console.log(b);
+  } else {
+    a = false;
+  }
+}
